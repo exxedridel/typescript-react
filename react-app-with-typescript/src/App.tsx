9d8651bf653ff1fde4dd668b476bb92b57657f9d
hevedrios <exxed.ridel@gmail.com>
2022-10-14 11:33:53 -0500

    Added: useState and renderizing with typescript

--- react-app-with-typescript/src/App.tsx
+++ react-app-with-typescript/src/App.tsx
@@ -1,36 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
-    // TypeScript is preventing from mixup types, but can be override like this
-  const [number, setNumber] = React.useState<number | string>(5)
-
-  const changeNumber = () => {
-    setNumber("2")
-  }
+  // when hovered subs state is showing that description can be undefined
+  // typescript stated the "contract" of the state
+  const [subs, setSubs] = React.useState([
+    {
+      nick: "dapelu",
+      subMonths: 3,
+      avatar: "https://i.pravatar.cc/150?u=dapelu",
+      description: "Dapelu hace de moderador a veces",
+    },
+    {
+      nick: "sergio_serrano",
+      subMonths: 7,
+      avatar: "https://i.pravatar.cc/150?u=sergio_serrano",
+    },
+  ]);
 
   return (
     <div className="App">
-
-      <header className='App-header'>
-        <h1>{number}</h1>
-        <button onClick={changeNumber}>Change number</button>
+      <header className="App-header">
+        <h1>Heved subs</h1>
+        <ul>
+          {subs.map((sub) => {
+            return (
+              // typescript provide better auto-complete
+              <li key={sub.nick}>
+                <img src={sub.avatar} alt={"Avatar for ${sub.nick}"} />
+                <h4>
+                  {sub.nick} (<small>{sub.subMonths}</small>)
+                </h4>
+                {/* typescript auto corrects when description needed ? sing for undefined property */}
+                <p>{sub.description?.substring(0, 21)}{sub.description ? "..." : ""}</p>
+              </li>
+            );
+          })}
+        </ul>
       </header>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
